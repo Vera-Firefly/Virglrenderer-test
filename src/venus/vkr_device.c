Vera-Firefly/Virglrenderer-test

commit d66378dbb47287f457d1e1a17d781ac7187f8f3d

vkr: init proc tables
Adds the code to make use of the new proc table functions
in the [physical_]device initialization.

Co-authored-by: Igor Torrente <igor.torrente@collabora.com>
Co-authored-by: Chia-I Wu <olvaffe@gmail.com>
Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/719>

src/venus/vkr_device.c
@@ -220,6 +220,19 @@ vkr_device_init_entry_points(struct vkr_device *dev, uint32_t api_version)
 }
 
 static void
+vkr_device_init_proc_table(struct vkr_device *dev,
+                           uint32_t api_version,
+                           const char *const *exts,
+                           uint32_t count)
+{
+   struct vn_info_extension_table ext_table;
+   vkr_extension_table_init(&ext_table, exts, count);
+
+   vn_util_init_device_proc_table(dev->base.handle.device, api_version, &ext_table,
+                                  &dev->proc_table);
+}
+
+static void
 vkr_dispatch_vkCreateDevice(struct vn_dispatch_context *dispatch,
                             struct vn_command_vkCreateDevice *args)
 {
@@ -272,9 +285,11 @@ vkr_dispatch_vkCreateDevice(struct vn_dispatch_context *dispatch,
       return;
    }
 
-   free(exts);
-
    dev->physical_device = physical_dev;
+
+   vkr_device_init_proc_table(dev, physical_dev->api_version, exts, ext_count);
+
+   free(exts);
 
    args->ret = vkr_device_create_queues(ctx, dev, args->pCreateInfo->queueCreateInfoCount,
                                         args->pCreateInfo->pQueueCreateInfos);

src/venus/vkr_common.c
@@ -143,6 +143,19 @@ vkr_log(const char *fmt, ...)
    virgl_log(line);
 }
 
+void
+vkr_extension_table_init(struct vn_info_extension_table *table,
+                         const char *const *exts,
+                         uint32_t count)
+{
+   memset(table, 0, sizeof(*table));
+   for (uint32_t i = 0; i < count; i++) {
+      const struct vn_info_extension *ext = vn_info_extension_get(exts[i]);
+      if (ext)
+         table->enabled[ext->index] = true;
+   }
+}
+
 uint32_t
 vkr_extension_get_spec_version(const char *name)
 {

src/venus/vkr_common.h
@@ -59,6 +59,7 @@
       .begin = (offset), .end = (offset) + (size)                                        \
    }
 
+struct vn_info_extension_table;
 struct vkr_context;
 struct vkr_instance;
 struct vkr_physical_device;
@@ -165,6 +166,11 @@ vkr_api_version_cap_minor(uint32_t version, uint32_t cap)
       version = cap - VK_API_VERSION_PATCH(cap) + VK_API_VERSION_PATCH(version);
    return version;
 }
+
+void
+vkr_extension_table_init(struct vn_info_extension_table *table,
+                         const char *const *exts,
+                         uint32_t count);
 
 uint32_t
 vkr_extension_get_spec_version(const char *name);

src/venus/vkr_device.h
@@ -6,14 +6,17 @@
 #ifndef VKR_DEVICE_H
 #define VKR_DEVICE_H
 
-#include "vkr_common.h"
+#include "venus-protocol/vn_protocol_renderer_util.h"
 
+#include "vkr_common.h"
 #include "vkr_context.h"
 
 struct vkr_device {
    struct vkr_object base;
 
    struct vkr_physical_device *physical_device;
+
+   struct vn_device_proc_table proc_table;
 
    /* Vulkan 1.2 */
    PFN_vkGetSemaphoreCounterValue GetSemaphoreCounterValue;

src/venus/vkr_physical_device.c
@@ -225,6 +225,14 @@ vkr_physical_device_init_properties(struct vkr_physical_device *physical_dev)
 }
 
 static void
+vkr_physical_device_init_proc_table(struct vkr_physical_device *physical_dev,
+                                    struct vkr_instance *instance)
+{
+   vn_util_init_physical_device_proc_table(instance->base.handle.instance,
+                                           &physical_dev->proc_table);
+}
+
+static void
 vkr_dispatch_vkEnumeratePhysicalDevices(struct vn_dispatch_context *dispatch,
                                         struct vn_command_vkEnumeratePhysicalDevices *args)
 {
@@ -281,6 +289,7 @@ vkr_dispatch_vkEnumeratePhysicalDevices(struct vn_dispatch_context *dispatch,
 
       physical_dev->base.handle.physical_device = instance->physical_device_handles[i];
 
+      vkr_physical_device_init_proc_table(physical_dev, instance);
       vkr_physical_device_init_properties(physical_dev);
       physical_dev->api_version =
          MIN2(physical_dev->properties.apiVersion, instance->api_version);

src/venus/vkr_physical_device.h
@@ -8,10 +8,14 @@
 
 #include "vkr_common.h"
 
+#include "venus-protocol/vn_protocol_renderer_util.h"
+
 struct gbm_device;
 
 struct vkr_physical_device {
    struct vkr_object base;
+
+   struct vn_physical_device_proc_table proc_table;
 
    VkPhysicalDeviceProperties properties;
    uint32_t api_version;

commit 8fa2e530b682b74ee228352e6e6ca29c0dcd81c4

venus: Fixes vkr_dispatch_vkCreateDevice that exts maybe NULL
Fixes: d66378d ("vkr: init proc tables")

exts maybe NULL when ext_count <= args->pCreateInfo->enabledExtensionCount

Signed-off-by: Yonggang Luo <luoyonggang@gmail.com>
Reviewed-by: Yiwei Zhang <zzyiwei@chromium.org>
Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/760>

src/venus/vkr_device.c
@@ -130,7 +130,9 @@ vkr_dispatch_vkCreateDevice(struct vn_dispatch_context *dispatch,
 
    dev->physical_device = physical_dev;
 
-   vkr_device_init_proc_table(dev, physical_dev->api_version, exts, ext_count);
+   vkr_device_init_proc_table(dev, physical_dev->api_version,
+                              args->pCreateInfo->ppEnabledExtensionNames,
+                              args->pCreateInfo->enabledExtensionCount);
 
    free(exts);
 