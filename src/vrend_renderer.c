a3741f1a75c612e4945ecbb3fcf7c098e91b5ad4
Gert Wollny <gert.wollny@collabora.com>
2022-03-24 16:29:45 +0100

    vrend: Always enable enhanced layouts
    Since we no longer emit enhanced layouts on the host and handle
    overlapping IO values and arrays by mapping them to vec4 values
    and arrays we can enable enhanced layouts in the guest.
    
    Some of the piglits related to arb_enhanced_layouts still fail,
    on GLES hosts, but these actually require ARB_vertex_attrib_64bit,
    which is something we lie about when running on GLES hosts.
    
    v2: remove the comment about limitations to enhanced layouts
    
    Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/766>

--- src/vrend_renderer.c
+++ src/vrend_renderer.c
@@ -10963,10 +10963,7 @@ static void vrend_renderer_fill_caps_v2(int gl_ver, int gles_ver,  union virgl_c
    if (has_feature(feat_texture_barrier))
       caps->v2.capability_bits |= VIRGL_CAP_TEXTURE_BARRIER;
 
-   /* If we enable input arrays and don't have enhanced layouts then we
-    * can't support components. */
-   if (has_feature(feat_enhanced_layouts))
-      caps->v2.capability_bits |= VIRGL_CAP_TGSI_COMPONENTS;
+   caps->v2.capability_bits |= VIRGL_CAP_TGSI_COMPONENTS;
 
    if (has_feature(feat_srgb_write_control))
       caps->v2.capability_bits |= VIRGL_CAP_SRGB_WRITE_CONTROL;
