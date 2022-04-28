494efa0843c768c8c8c33975d43a8e99d718d754
Rob Clark <robdclark@chromium.org>
2022-04-28 13:20:49 -0700

    drm: Import linux overflow helpers
    The kernel has some already well-considered helpers for dealing with
    size checks vs overflow.  Lets use that.
    
    Signed-off-by: Rob Clark <robdclark@chromium.org>
    Reviewed-by: Chia-I Wu <olvaffe@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/795>

--- src/drm/drm_util.h
+++ src/drm/drm_util.h
@@ -8,6 +8,10 @@
 
 #pragma GCC diagnostic ignored "-Wgnu-zero-variadic-macro-arguments"
 #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
+#pragma GCC diagnostic ignored "-Wlanguage-extension-token"
+#pragma GCC diagnostic ignored "-Wgnu-statement-expression"
+
+#include "linux/overflow.h"
 
 void _drm_log(const char *fmt, ...);
 #define drm_log(fmt, ...) _drm_log("%s:%d: " fmt, __func__, __LINE__, ##__VA_ARGS__)
