Vera-Firefly/Virglrenderer-test

commit 48525f0e7f85ad63e349bfa85c9c266eeaaef6c2

vrend: Track sub-ctx in queries
Add the sub context id to the query structure and set it current before
checking the query. This is necessary as switching context is not enough
to restore the GL context associated to a query object.

Signed-off-by: Antonio Caggiano <antonio.caggiano@collabora.com>
Reviewed-by: Ryan Neph <ryanneph@google.com>
Reviewed-by: Gert Wollny <gert.wollny@collabora.com>
Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/848>

src/vrend_renderer.c
@@ -107,6 +107,7 @@ struct vrend_query {
    GLuint index;
    GLuint gltype;
    struct vrend_context *ctx;
+   int sub_ctx_id;
    struct vrend_resource *res;
    bool fake_samples_passed;
 };
@@ -10081,14 +10082,56 @@ static bool vrend_check_query(struct vrend_query *query)
    return true;
 }
 
+static struct vrend_sub_context *vrend_renderer_find_sub_ctx(struct vrend_context *ctx,
+                                                             int sub_ctx_id)
+{
+   struct vrend_sub_context *sub;
+
+   if (ctx->sub && ctx->sub->sub_ctx_id == sub_ctx_id)
+      return ctx->sub;
+
+   LIST_FOR_EACH_ENTRY(sub, &ctx->sub_ctxs, head) {
+      if (sub->sub_ctx_id == sub_ctx_id)
+         return sub;
+   }
+
+   return NULL;
+}
+
+static bool vrend_hw_switch_context_with_sub(struct vrend_context *ctx, int sub_ctx_id)
+{
+   if (!ctx)
+      return false;
+
+   if (ctx == vrend_state.current_ctx && sub_ctx_id == ctx->sub->sub_ctx_id &&
+       ctx->ctx_switch_pending == false) {
+      return true;
+   }
+
+   if (ctx->ctx_id != 0 && ctx->in_error)
+      return false;
+
+   struct vrend_sub_context *sub = vrend_renderer_find_sub_ctx(ctx, sub_ctx_id);
+   if (!sub)
+      return false;
+
+   ctx->sub = sub;
+
+   ctx->ctx_switch_pending = true;
+   vrend_finish_context_switch(ctx);
+
+   vrend_state.current_ctx = ctx;
+   return true;
+}
+
 static void vrend_renderer_check_queries(void)
 {
    struct vrend_query *query, *stor;
 
    LIST_FOR_EACH_ENTRY_SAFE(query, stor, &vrend_state.waiting_query_list, waiting_queries) {
-      if (!vrend_hw_switch_context(query->ctx, true)) {
-         vrend_printf("failed to switch to context (%d) for query %u\n",
-                      query->ctx->ctx_id, query->id);
+      if (!vrend_hw_switch_context_with_sub(query->ctx, query->sub_ctx_id)) {
+         vrend_printf("failed to switch to context (%d) with sub (%d) for query %u\n",
+                      query->ctx->ctx_id, query->sub_ctx_id, query->id);
       }
       else if (!vrend_check_query(query)) {
          continue;
@@ -10183,6 +10226,7 @@ int vrend_create_query(struct vrend_context *ctx, uint32_t handle,
    q->type = query_type;
    q->index = query_index;
    q->ctx = ctx;
+   q->sub_ctx_id = ctx->sub->sub_ctx_id;
    q->fake_samples_passed = fake_samples_passed;
 
    vrend_resource_reference(&q->res, res);
@@ -11582,18 +11626,10 @@ void vrend_renderer_destroy_sub_ctx(struct vrend_context *ctx, int sub_ctx_id)
 
 void vrend_renderer_set_sub_ctx(struct vrend_context *ctx, int sub_ctx_id)
 {
-   struct vrend_sub_context *sub;
-   /* find the sub ctx */
-
-   if (ctx->sub && ctx->sub->sub_ctx_id == sub_ctx_id)
-      return;
-
-   LIST_FOR_EACH_ENTRY(sub, &ctx->sub_ctxs, head) {
-      if (sub->sub_ctx_id == sub_ctx_id) {
-         ctx->sub = sub;
-         vrend_clicbs->make_current(sub->gl_context);
-         break;
-      }
+   struct vrend_sub_context *sub = vrend_renderer_find_sub_ctx(ctx, sub_ctx_id);
+   if (sub && ctx->sub != sub) {
+      ctx->sub = sub;
+      vrend_clicbs->make_current(sub->gl_context);
    }
 }
 

commit c85335e9d1d15bb1d9d9fa357c7e027e1713e24e

vrend: force gl context switch when sub context changes in query check
vrend_state.current_hw_context must be invalidated when query checking
requires a sub context switch, otherwise gl context switching
doesn't actually occur.

Fixes: 48525f0e ("vrend: Track sub-ctx in queries")

Signed-off-by: Ryan Neph <ryanneph@google.com>
Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/858>

src/vrend_renderer.c
@@ -10115,7 +10115,11 @@ static bool vrend_hw_switch_context_with_sub(struct vrend_context *ctx, int sub_
    if (!sub)
       return false;
 
-   ctx->sub = sub;
+   /* force the gl context switch to occur */
+   if (ctx->sub != sub) {
+      vrend_state.current_hw_ctx = NULL;
+      ctx->sub = sub;
+   }
 
    ctx->ctx_switch_pending = true;
    vrend_finish_context_switch(ctx);