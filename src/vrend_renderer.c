834502e3c8855333501d988913c49f31e6ce1c4a
Gert Wollny <gert.wollny@collabora.com>
2022-11-08 14:24:43 +0100

    vrend: simplify set_scissor_state function
    That return in the middle of the function doesn't help here
    and makes the logic more difficult to understand.
    
    Related Coverity ID: 1527211 "Out-of-bounds write"
    
    Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/975>

--- src/vrend_renderer.c
+++ src/vrend_renderer.c
@@ -9380,19 +9380,15 @@ void vrend_set_scissor_state(struct vrend_context *ctx,
                              uint32_t num_scissor,
                              struct pipe_scissor_state *ss)
 {
-   uint i, idx;
-
-   if (start_slot > PIPE_MAX_VIEWPORTS ||
-       num_scissor > (PIPE_MAX_VIEWPORTS - start_slot)) {
+   if (start_slot < PIPE_MAX_VIEWPORTS &&
+       start_slot + num_scissor <= PIPE_MAX_VIEWPORTS) {
+      for (uint i = 0; i < num_scissor; i++) {
+         uint idx = start_slot + i;
+         ctx->sub->ss[idx] = ss[i];
+         ctx->sub->scissor_state_dirty |= (1 << idx);
+      }
+   } else
       vrend_report_buffer_error(ctx, 0);
-      return;
-   }
-
-   for (i = 0; i < num_scissor; i++) {
-      idx = start_slot + i;
-      ctx->sub->ss[idx] = ss[i];
-      ctx->sub->scissor_state_dirty |= (1 << idx);
-   }
 }
 
 void vrend_set_polygon_stipple(struct vrend_context *ctx,
