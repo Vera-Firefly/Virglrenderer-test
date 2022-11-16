Vera-Firefly/Virglrenderer-test

commit 2f6aa408882cb12b152c6abb2b71e37251feb8cf

shader: require glsl 1.40 when using a sysvalue UBO
Using an UBO with std140 needs GLSL 1.40

Closes #294

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/978>

src/vrend_shader.c
@@ -7875,6 +7875,10 @@ bool vrend_convert_shader(const struct vrend_context *rctx,
    if (cfg->use_core_profile && cfg->glsl_version >= 140)
       ctx.glsl_ver_required = require_glsl_ver(&ctx, 140);
 
+   /* If we need a sysvalue UBO then we require GLSL 1.40 */
+   if (ctx.glsl_strbufs.required_sysval_uniform_decls)
+      ctx.glsl_ver_required = require_glsl_ver(&ctx, 140);
+
    if (sinfo->so_info.num_outputs) {
       ctx.so = &sinfo->so_info;
       ctx.so_names = calloc(sinfo->so_info.num_outputs, sizeof(char *));

commit 98be3d1adfb8c37340b6abfe5edfbd4183fae802

shader: force setting the shader version after translating the shader
Only after translating the shader we actually know whether the
system UBO is used, and hence, the minimum shader version needs
to be set.

Fixes: 2f6aa408882cb
   shader: require glsl 1.40 when using a sysvalue UBO

Closes #294

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/980>

src/vrend_shader.c
@@ -7884,10 +7884,6 @@ bool vrend_convert_shader(const struct vrend_context *rctx,
    if (cfg->use_core_profile && cfg->glsl_version >= 140)
       ctx.glsl_ver_required = require_glsl_ver(&ctx, 140);
 
-   /* If we need a sysvalue UBO then we require GLSL 1.40 */
-   if (ctx.glsl_strbufs.required_sysval_uniform_decls)
-      ctx.glsl_ver_required = require_glsl_ver(&ctx, 140);
-
    if (sinfo->so_info.num_outputs) {
       ctx.so = &sinfo->so_info;
       ctx.so_names = calloc(sinfo->so_info.num_outputs, sizeof(char *));
@@ -7913,6 +7909,10 @@ bool vrend_convert_shader(const struct vrend_context *rctx,
    bret = tgsi_iterate_shader(tokens, &ctx.iter);
    if (bret == false)
       goto fail;
+
+   /* If we need a sysvalue UBO then we require GLSL 1.40 */
+   if (ctx.glsl_strbufs.required_sysval_uniform_decls)
+      ctx.glsl_ver_required = require_glsl_ver(&ctx, 140);
 
    if (!ctx.cfg->use_gles &&
       ( key->in_arrays.num_arrays > 0 ) &&