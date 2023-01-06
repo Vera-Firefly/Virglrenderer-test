Vera-Firefly/Virglrenderer-test

commit 5de7773226c9b969a5ff853bf0713a994eb2cffa

vkr: use sync_queue mapping in context_submit_fence
vkr_context_submit_fence creates a VkFence on the VkQueue bound to the
sync_queue identified by ring_idx during renderer submission. If the
ring_idx = 0, the guest kernel fence is immediately signaled instead.

Signed-off-by: Ryan Neph <ryanneph@google.com>
Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/982>

src/venus/vkr_context.c
@@ -120,6 +120,22 @@ vkr_context_init_dispatch(struct vkr_context *ctx)
    vkr_context_init_command_buffer_dispatch(ctx);
 }
 
+static struct vkr_cpu_sync *
+vkr_alloc_cpu_sync(uint32_t flags, uint32_t ring_idx, uint64_t fence_id)
+{
+   struct vkr_cpu_sync *sync;
+   sync = malloc(sizeof(*sync));
+   if (!sync)
+      return NULL;
+
+   sync->flags = flags;
+   sync->fence_id = fence_id;
+   sync->ring_idx = ring_idx;
+   list_inithead(&sync->head);
+
+   return sync;
+}
+
 static int
 vkr_context_submit_fence_locked(struct virgl_context *base,
                                 uint32_t flags,
@@ -127,15 +143,32 @@ vkr_context_submit_fence_locked(struct virgl_context *base,
                                 uint64_t fence_id)
 {
    struct vkr_context *ctx = (struct vkr_context *)base;
-   struct vn_device_proc_table *vk;
-   struct vkr_queue *queue;
    VkResult result;
 
-   queue = vkr_context_get_object(ctx, ring_idx);
-   if (!queue)
+   if (ring_idx >= ARRAY_SIZE(ctx->sync_queues)) {
+      vkr_log("invalid sync ring_idx %u", ring_idx);
       return -EINVAL;
+   }
+
+   if (ring_idx == 0) {
+      if (vkr_renderer_flags & VKR_RENDERER_ASYNC_FENCE_CB) {
+         ctx->base.fence_retire(&ctx->base, ring_idx, fence_id);
+      } else {
+         struct vkr_cpu_sync *sync = vkr_alloc_cpu_sync(flags, ring_idx, fence_id);
+         if (!sync)
+            return -ENOMEM;
+
+         list_addtail(&sync->head, &ctx->signaled_cpu_syncs);
+      }
+      return 0;
+   } else if (!ctx->sync_queues[ring_idx]) {
+      vkr_log("invalid ring_idx %u", ring_idx);
+      return -EINVAL;
+   }
+
+   struct vkr_queue *queue = ctx->sync_queues[ring_idx];
    struct vkr_device *dev = queue->device;
-   vk = &dev->proc_table;
+   struct vn_device_proc_table *vk = &dev->proc_table;
 
    struct vkr_queue_sync *sync =
       vkr_device_alloc_queue_sync(dev, flags, ring_idx, fence_id);
@@ -174,6 +207,10 @@ vkr_context_submit_fence(struct virgl_context *base,
    struct vkr_context *ctx = (struct vkr_context *)base;
    int ret;
 
+   /* always merge fences */
+   assert(!(flags & ~VIRGL_RENDERER_FENCE_FLAG_MERGEABLE));
+   flags = VIRGL_RENDERER_FENCE_FLAG_MERGEABLE;
+
    mtx_lock(&ctx->mutex);
    ret = vkr_context_submit_fence_locked(base, flags, ring_idx, fence_id);
    mtx_unlock(&ctx->mutex);
@@ -196,6 +233,14 @@ vkr_context_retire_fences_locked(struct virgl_context *base)
       free(sync);
    }
    list_inithead(&ctx->signaled_syncs);
+
+   /* retire syncs from CPU timeline */
+   struct vkr_cpu_sync *cpu_sync, *cpu_sync_tmp;
+   LIST_FOR_EACH_ENTRY_SAFE (cpu_sync, cpu_sync_tmp, &ctx->signaled_cpu_syncs, head) {
+      ctx->base.fence_retire(&ctx->base, cpu_sync->ring_idx, cpu_sync->fence_id);
+      free(cpu_sync);
+   }
+   list_inithead(&ctx->signaled_cpu_syncs);
 
    /* flush first and once because the per-queue sync threads might write to
     * it any time
@@ -538,6 +583,10 @@ vkr_context_destroy(struct virgl_context *base)
    LIST_FOR_EACH_ENTRY_SAFE (sync, tmp, &ctx->signaled_syncs, head)
       free(sync);
 
+   struct vkr_queue_sync *cpu_sync, *cpu_sync_tmp;
+   LIST_FOR_EACH_ENTRY_SAFE (cpu_sync, cpu_sync_tmp, &ctx->signaled_cpu_syncs, head)
+      free(cpu_sync);
+
    if (ctx->fence_eventfd >= 0)
       close(ctx->fence_eventfd);
 
@@ -645,6 +694,7 @@ vkr_context_create(size_t debug_len, const char *debug_name)
    list_inithead(&ctx->rings);
    list_inithead(&ctx->busy_queues);
    list_inithead(&ctx->signaled_syncs);
+   list_inithead(&ctx->signaled_cpu_syncs);
 
    return &ctx->base;
 

src/venus/vkr_context.h
@@ -42,6 +42,14 @@ enum vkr_context_validate_level {
    VKR_CONTEXT_VALIDATE_FULL,
 };
 
+struct vkr_cpu_sync {
+   uint32_t flags;
+   uint32_t ring_idx;
+   uint64_t fence_id;
+
+   struct list_head head;
+};
+
 struct vkr_context {
    struct virgl_context base;
 
@@ -62,6 +70,7 @@ struct vkr_context {
    int fence_eventfd;
    struct list_head busy_queues;
    struct list_head signaled_syncs;
+   struct list_head signaled_cpu_syncs;
 
    struct vkr_queue *sync_queues[64];
 

src/virglrenderer.c
@@ -605,11 +605,29 @@ void *virgl_renderer_get_cursor_data(uint32_t resource_id, uint32_t *width, uint
                                              height);
 }
 
+static bool
+virgl_context_foreach_retire_fences(struct virgl_context *ctx,
+                                    UNUSED void* data)
+{
+   /* vrend contexts are polled explicitly by the caller */
+   if (ctx->capset_id != VIRGL_RENDERER_CAPSET_VIRGL &&
+       ctx->capset_id != VIRGL_RENDERER_CAPSET_VIRGL2)
+   {
+      assert(ctx->retire_fences);
+      ctx->retire_fences(ctx);
+   }
+   return true;
+}
+
 void virgl_renderer_poll(void)
 {
    TRACE_FUNC();
    if (state.vrend_initialized)
       vrend_renderer_poll();
+
+   struct virgl_context_foreach_args args;
+   args.callback = virgl_context_foreach_retire_fences;
+   virgl_context_foreach(&args);
 }
 
 void virgl_renderer_cleanup(UNUSED void *cookie)

commit f1e2627a113d45cd5c82653bb3190040c5e673aa

vkr: fix cpu sync cleanup at context destroy
Loop variable had the wrong type, so list iteration was bugged.

Signed-off-by: Ryan Neph <ryanneph@google.com>
Fixes: 5de7773226c9b ("vkr: use sync_queue mapping in context_submit_fence")
Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/1010>

src/venus/vkr_context.c
@@ -583,7 +583,7 @@ vkr_context_destroy(struct virgl_context *base)
    LIST_FOR_EACH_ENTRY_SAFE (sync, tmp, &ctx->signaled_syncs, head)
       free(sync);
 
-   struct vkr_queue_sync *cpu_sync, *cpu_sync_tmp;
+   struct vkr_cpu_sync *cpu_sync, *cpu_sync_tmp;
    LIST_FOR_EACH_ENTRY_SAFE (cpu_sync, cpu_sync_tmp, &ctx->signaled_cpu_syncs, head)
       free(cpu_sync);
 