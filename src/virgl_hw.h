8df4cba170940dad9350a99900293adbcef39b6c
Honglei Huang <honglei1.huang@amd.com>
2023-01-12 18:07:21 +0800

    video: Add YNV pipe format in virgl format table
    Add Y8_400, Y8_U8_V8_444 into virgl_formats to fix the
    jpeg virgl codec fail issue caused by resource creating error.
    Add VIRGL_FORMAT_R8G8_R8B8_UNORM into base_rgba_formats vrend_format_table
    to fix the jpeg virgl codec fail issue caused by lost of above type in
    tex_conv_table called by function vrend_resource_alloc_texture.
    
    Signed-off-by: Honglei Huang <honglei1.huang@amd.com>
    Reviewed-by: Daniel Almeida <daniel.almeida@collabora.com>
    Reviewed-by: Corentin Noël <corentin.noel@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/1068>

--- src/virgl_hw.h
+++ src/virgl_hw.h
@@ -408,6 +408,7 @@ enum virgl_formats {
    VIRGL_FORMAT_R4G4B4A4_UNORM          = 319,
    VIRGL_FORMAT_R5G5B5A1_UNORM          = 320,
    VIRGL_FORMAT_R5G6B5_UNORM            = 321,
+   VIRGL_FORMAT_Y8_400_UNORM            = 322,
 
    VIRGL_FORMAT_MAX /* = PIPE_FORMAT_COUNT */,
 
