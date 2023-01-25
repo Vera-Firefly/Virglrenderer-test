Vera-Firefly/Virglrenderer-test

commit 59449ad4d16d36c44f4e4088ec62382fb919e716

virgl: add VIRGL_RENDERER_BLOB_FD_TYPE_SHM
This allows a blob resource to be backed by a mmap()-able fd.
Internally, it is a memfd or a shm.

Signed-off-by: Chia-I Wu <olvaffe@gmail.com>
Reviewed-by: Yiwei Zhang <zzyiwei@chromium.org>
Reviewed-by: Ryan Neph <ryanneph@google.com>

src/virglrenderer.c
@@ -925,6 +925,7 @@ int virgl_renderer_resource_map(uint32_t res_handle, void **out_map, uint64_t *o
    } else {
       switch (res->fd_type) {
       case VIRGL_RESOURCE_FD_DMABUF:
+      case VIRGL_RESOURCE_FD_SHM:
          map = mmap(NULL, res->map_size, PROT_WRITE | PROT_READ, MAP_SHARED, res->fd, 0);
          break;
       case VIRGL_RESOURCE_FD_OPAQUE:
@@ -990,6 +991,9 @@ virgl_renderer_resource_export_blob(uint32_t res_id, uint32_t *fd_type, int *fd)
       break;
    case VIRGL_RESOURCE_FD_OPAQUE:
       *fd_type = VIRGL_RENDERER_BLOB_FD_TYPE_OPAQUE;
+      break;
+   case VIRGL_RESOURCE_FD_SHM:
+      *fd_type = VIRGL_RENDERER_BLOB_FD_TYPE_SHM;
       break;
    default:
       return EINVAL;

src/virgl_resource.h
@@ -33,6 +33,8 @@ struct pipe_resource;
 enum virgl_resource_fd_type {
    VIRGL_RESOURCE_FD_DMABUF,
    VIRGL_RESOURCE_FD_OPAQUE,
+   /* mmap()-able, usually memfd or shm */
+   VIRGL_RESOURCE_FD_SHM,
 
    VIRGL_RESOURCE_FD_INVALID = -1,
 };

src/virglrenderer.h
@@ -323,6 +323,7 @@ VIRGL_EXPORT int virgl_renderer_resource_get_map_info(uint32_t res_handle, uint3
 
 #define VIRGL_RENDERER_BLOB_FD_TYPE_DMABUF        0x0001
 #define VIRGL_RENDERER_BLOB_FD_TYPE_OPAQUE        0x0002
+#define VIRGL_RENDERER_BLOB_FD_TYPE_SHM           0x0003
 
 VIRGL_EXPORT int
 virgl_renderer_resource_export_blob(uint32_t res_id, uint32_t *fd_type, int *fd);

vtest/vtest_renderer.c
@@ -1192,7 +1192,7 @@ int vtest_resource_create_blob(UNUSED uint32_t length_dw)
       return report_failed_call("virgl_renderer_resource_create_blob", ret);
    }
 
-   /* need dmabuf */
+   /* export blob */
    if (args.blob_mem == VIRGL_RENDERER_BLOB_MEM_HOST3D) {
       uint32_t fd_type;
       ret = virgl_renderer_resource_export_blob(res->res_id, &fd_type, &fd);
@@ -1200,7 +1200,8 @@ int vtest_resource_create_blob(UNUSED uint32_t length_dw)
          vtest_unref_resource(res);
          return report_failed_call("virgl_renderer_resource_export_blob", ret);
       }
-      if (fd_type != VIRGL_RENDERER_BLOB_FD_TYPE_DMABUF) {
+      if (fd_type != VIRGL_RENDERER_BLOB_FD_TYPE_DMABUF &&
+          fd_type != VIRGL_RENDERER_BLOB_FD_TYPE_SHM) {
          close(fd);
          vtest_unref_resource(res);
          return report_failed_call("virgl_renderer_resource_export_blob", -EINVAL);

commit 65d8f9bc16492db77637a2d2e09fdab5aab2ccbe

virgl: unmap shm resource to avoid leak
When sandbox is disabled, resource map/unmap is done by virglrenderer.
Ensure to also unmap the shm resource.

Fixes: 59449ad ("virgl: add VIRGL_RENDERER_BLOB_FD_TYPE_SHM")

Signed-off-by: Yiwei Zhang <zzyiwei@chromium.org>
Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/1029>

src/virglrenderer.c
@@ -1089,6 +1089,7 @@ int virgl_renderer_resource_unmap(uint32_t res_handle)
    } else {
       switch (res->fd_type) {
       case VIRGL_RESOURCE_FD_DMABUF:
+      case VIRGL_RESOURCE_FD_SHM:
          ret = munmap(res->mapped, res->map_size);
          break;
       case VIRGL_RESOURCE_FD_OPAQUE: