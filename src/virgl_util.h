Vera-Firefly/Virglrenderer-test

commit bfa5b59350a1d2cac320f6a7d1cabce9583de1c1

util: Fix trace_scope macros to always return and consume the scope object
Provide a consistent use of the scope in the different definitions of the macros.

This is a first step in making sure that we are always feeding back to trace_end the
result of trace_begin.

Signed-off-by: Corentin Noël <corentin.noel@collabora.com>
Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/1132>

src/virgl_util.h
@@ -112,8 +112,8 @@ void trace_end(const char **scope);
 
 #define TRACE_SCOPE_SLOW(SCOPE) TRACE_SCOPE(SCOPE)
 
-#define TRACE_SCOPE_BEGIN(SCOPE) trace_begin(SCOPE);
-#define TRACE_SCOPE_END(SCOPE)  trace_end(NULL);
+#define TRACE_SCOPE_BEGIN(SCOPE) trace_begin(SCOPE)
+#define TRACE_SCOPE_END(SCOPE_OBJ)  trace_end(&SCOPE_OBJ)
 
 #endif /* ENABLE_TRACING == TRACE_WITH_PERCETTO */
 
@@ -122,8 +122,8 @@ void trace_end(const char **scope);
 #define TRACE_FUNC()
 #define TRACE_SCOPE(SCOPE)
 #define TRACE_SCOPE_SLOW(SCOPE)
-#define TRACE_SCOPE_BEGIN(SCOPE)
-#define TRACE_SCOPE_END(VAR)
+#define TRACE_SCOPE_BEGIN(SCOPE) NULL
+#define TRACE_SCOPE_END(SCOPE_OBJ) (void)SCOPE_OBJ
 #endif /* ENABLE_TRACING */
 
 #endif /* VIRGL_UTIL_H */

src/drm/msm/msm_renderer.c
@@ -792,13 +792,11 @@ msm_ccmd_gem_set_iova(struct msm_context *mctx, const struct msm_ccmd_req *hdr)
 
    uint64_t iova = req->iova;
    if (iova) {
-      TRACE_SCOPE_BEGIN("SET_IOVA");
+      TRACE_SCOPE("SET_IOVA");
       ret = gem_info(mctx, obj->handle, MSM_INFO_SET_IOVA, &iova);
-      TRACE_SCOPE_END("SET_IOVA");
    } else {
-      TRACE_SCOPE_BEGIN("CLEAR_IOVA");
+      TRACE_SCOPE("CLEAR_IOVA");
       ret = gem_info(mctx, obj->handle, MSM_INFO_SET_IOVA, &iova);
-      TRACE_SCOPE_END("CLEAR_IOVA");
    }
    if (ret) {
       drm_log("SET_IOVA failed: %d (%s)", ret, strerror(errno));
@@ -1136,7 +1134,7 @@ submit_cmd_dispatch(struct msm_context *mctx, const struct msm_ccmd_req *hdr)
    drm_dbg("%s: hdr={cmd=%u, len=%u, seqno=%u, rsp_off=0x%x)", ccmd->name, hdr->cmd,
            hdr->len, hdr->seqno, hdr->rsp_off);
 
-   TRACE_SCOPE_BEGIN(ccmd->name);
+   const char *trace_scope = TRACE_SCOPE_BEGIN(ccmd->name);
 
    /* If the request length from the guest is smaller than the expected
     * size, ie. newer host and older guest, we need to make a copy of
@@ -1153,7 +1151,7 @@ submit_cmd_dispatch(struct msm_context *mctx, const struct msm_ccmd_req *hdr)
       ret = ccmd->handler(mctx, hdr);
    }
 
-   TRACE_SCOPE_END(ccmd->name);
+   TRACE_SCOPE_END(trace_scope);
 
    if (ret) {
       drm_log("%s: dispatch failed: %d (%s)", ccmd->name, ret, strerror(errno));

commit ca25f5158b5aa3927357fcf505fe4e26e72d4f24

util: Fix percetto build
Fixes: bfa5b59350a1 ("util: Fix trace_scope macros to always return and consume the scope object")
Signed-off-by: Rob Clark <robdclark@chromium.org>
Reviewed-by: Dmitry Osipenko <dmitry.osipenko@collabora.com>
Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/1134>

src/virgl_util.h
@@ -98,7 +98,7 @@ PERCETTO_CATEGORY_DECLARE(VIRGL_PERCETTO_CATEGORIES)
 /* Trace high frequency events (tracing may impact performance). */
 #define TRACE_SCOPE_SLOW(SCOPE) TRACE_EVENT(virgls, SCOPE)
 
-#define TRACE_SCOPE_BEGIN(SCOPE) TRACE_EVENT_BEGIN(virgl, SCOPE)
+#define TRACE_SCOPE_BEGIN(SCOPE) ({TRACE_EVENT_BEGIN(virgl, SCOPE); NULL;})
 #define TRACE_SCOPE_END(SCOPE) do { TRACE_EVENT_END(virgl); (void)SCOPE; } while (0)
 
 #else