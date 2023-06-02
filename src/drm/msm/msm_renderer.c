Vera-Firefly/Virglrenderer-test

commit 3c1980b4bdf26a024a7c87eee279f855b8b02f35

util: Make trace_begin and trace_end use an anonymous pointer
Allows to hide the tracing implementation from the consumer side.
Other frameworks like sysprof are using a time+duration model instead
of a start+end model, this allows them to be implementable.

Signed-off-by: Corentin Noël <corentin.noel@collabora.com>
Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/983>

src/drm/msm/msm_renderer.c
@@ -1134,7 +1134,7 @@ submit_cmd_dispatch(struct msm_context *mctx, const struct msm_ccmd_req *hdr)
    drm_dbg("%s: hdr={cmd=%u, len=%u, seqno=%u, rsp_off=0x%x)", ccmd->name, hdr->cmd,
            hdr->len, hdr->seqno, hdr->rsp_off);
 
-   const char *trace_scope = TRACE_SCOPE_BEGIN(ccmd->name);
+   void *scope = TRACE_SCOPE_BEGIN(ccmd->name);
 
    /* If the request length from the guest is smaller than the expected
     * size, ie. newer host and older guest, we need to make a copy of

src/virgl_util.c
@@ -198,13 +198,13 @@ void trace_init(void)
    vperfetto_min_startTracing(&config);
 }
 
-const char *trace_begin(const char *scope)
+void *trace_begin(const char *scope)
 {
    vperfetto_min_beginTrackEvent_VMM(scope);
-   return scope;
+   return NULL;
 }
 
-void trace_end(const char **dummy)
+void trace_end(void **dummy)
 {
    (void)dummy;
    vperfetto_min_endTrackEvent_VMM();
@@ -217,7 +217,7 @@ void trace_init(void)
 {
 }
 
-const char *trace_begin(const char *scope)
+void *trace_begin(const char *scope)
 {
    for (int i = 0; i < nesting_depth; ++i)
       fprintf(stderr, "  ");
@@ -225,14 +225,14 @@ const char *trace_begin(const char *scope)
    fprintf(stderr, "ENTER:%s\n", scope);
    nesting_depth++;
 
-   return scope;
+   return (void *)scope;
 }
 
-void trace_end(const char **func_name)
+void trace_end(void **func_name)
 {
    --nesting_depth;
    for (int i = 0; i < nesting_depth; ++i)
       fprintf(stderr, "  ");
-   fprintf(stderr, "LEAVE %s\n", *func_name);
+   fprintf(stderr, "LEAVE %s\n", (const char *) *func_name);
 }
 #endif

src/virgl_util.h
@@ -103,11 +103,11 @@ PERCETTO_CATEGORY_DECLARE(VIRGL_PERCETTO_CATEGORIES)
 
 #else
 
-const char *trace_begin(const char *scope);
-void trace_end(const char **scope);
+void *trace_begin(const char *scope);
+void trace_end(void **scope);
 
 #define TRACE_SCOPE(SCOPE) \
-   const char *trace_dummy __attribute__((cleanup (trace_end), unused)) = \
+   void *trace_dummy __attribute__((cleanup (trace_end), unused)) = \
    trace_begin(SCOPE)
 
 #define TRACE_SCOPE_SLOW(SCOPE) TRACE_SCOPE(SCOPE)

src/vrend_renderer.c
@@ -12772,12 +12772,12 @@ void vrend_context_emit_string_marker(struct vrend_context *ctx, GLsizei length,
     if (length > 6 && !strncmp(message, "BEGIN:", 6)) {
        snprintf(buf, 256, "%.*s", length - 6, &message[6]);
        char *scope_name = ralloc_strndup(ctx->active_markers, buf, 256);
-       const char *scope = TRACE_SCOPE_BEGIN(scope_name);
+       void *scope = TRACE_SCOPE_BEGIN(scope_name);
        _mesa_hash_table_insert(ctx->active_markers, scope_name, (void *)scope);
     } else if (length > 4 && !strncmp(message, "END:", 4)) {
        snprintf(buf, 256, "%.*s", length - 4, &message[4]);
        struct hash_entry *entry = _mesa_hash_table_search(ctx->active_markers, buf);
-       const char *orig_scope = (const char *) entry->data;
+       void *orig_scope = entry->data;
        TRACE_SCOPE_END(orig_scope);
        ralloc_free((void *)entry->key);
       _mesa_hash_table_remove(ctx->active_markers, entry);

commit b7886b53e913bba5cd859404b479b0550dcd36d4

msm_renderer: Fix wrong variable name
Fixes: 3c1980b4bdf26a024a7c87eee279f855b8b02f35
Signed-off-by: Corentin Noël <corentin.noel@collabora.com>
Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/1136>

src/drm/msm/msm_renderer.c
@@ -1134,7 +1134,7 @@ submit_cmd_dispatch(struct msm_context *mctx, const struct msm_ccmd_req *hdr)
    drm_dbg("%s: hdr={cmd=%u, len=%u, seqno=%u, rsp_off=0x%x)", ccmd->name, hdr->cmd,
            hdr->len, hdr->seqno, hdr->rsp_off);
 
-   void *scope = TRACE_SCOPE_BEGIN(ccmd->name);
+   void *trace_scope = TRACE_SCOPE_BEGIN(ccmd->name);
 
    /* If the request length from the guest is smaller than the expected
     * size, ie. newer host and older guest, we need to make a copy of