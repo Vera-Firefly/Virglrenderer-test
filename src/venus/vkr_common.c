fdad618a5bd69885b5ce0555db22f60036b0b0ce
Juston Li <justonli@google.com>
2023-06-07 14:36:50 -0700

    vkr: enable VK_KHR_shader_clock
    Signed-off-by: Juston Li <justonli@google.com>
    Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/1146>

--- src/venus/vkr_common.c
+++ src/venus/vkr_common.c
@@ -97,6 +97,7 @@ static const struct vn_info_extension_table vkr_extension_table = {
    .KHR_external_memory_fd = true,
    .KHR_external_semaphore_fd = true,
    .KHR_push_descriptor = true,
+   .KHR_shader_clock = true,
    /* EXT extensions */
    .EXT_calibrated_timestamps = true,
    .EXT_conservative_rasterization = true,
