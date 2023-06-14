Vera-Firefly/Virglrenderer-test

commit 511f450642ffbf01fd96efe63c591c2317391b46

Fix vrend_resource_gbm_init() error handling
Do not set GBM_BUFFER | EGL_IMAGE until the completion of the function
to avoid inconsistency.

Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
Acked-by: Gert Wollny <gert.wollny@collabora.com>
Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/1103>

src/vrend_renderer.c
@@ -8071,7 +8071,6 @@ static void vrend_resource_gbm_init(struct vrend_resource *gr, uint32_t format)
       return;
 
    gr->gbm_bo = bo;
-   gr->storage_bits |= VREND_STORAGE_GBM_BUFFER;
    /* This is true so far, but maybe gbm_bo_get_caching_type is needed in the future. */
    if (!strcmp(gbm_device_get_backend_name(gbm->device), "i915"))
       gr->map_info = VIRGL_RENDERER_MAP_CACHE_CACHED;
@@ -8085,8 +8084,10 @@ static void vrend_resource_gbm_init(struct vrend_resource *gr, uint32_t format)
    if (!gr->egl_image) {
       gr->gbm_bo = NULL;
       gbm_bo_destroy(bo);
+      return;
    }
 
+   gr->storage_bits |= VREND_STORAGE_GBM_BUFFER;
    gr->storage_bits |= VREND_STORAGE_EGL_IMAGE;
 
 #else

commit 8f986932f5df8f8888a40db9b9662215faf3d346

vrend: fix missed flagging as GBM_BUFFER on early-return
A recent refactor incorrectly moved GBM_BUFFER flagging after a
non-error early-return in vrend_resource_gbm_init.

Fixes: 511f450642 ("Fix vrend_resource_gbm_init() error handling")

Signed-off-by: Ryan Neph <ryanneph@google.com>
Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/1162>

src/vrend_renderer.c
@@ -8256,6 +8256,8 @@ static void vrend_resource_gbm_init(struct vrend_resource *gr, uint32_t format)
       return;
 
    gr->gbm_bo = bo;
+   gr->storage_bits |= VREND_STORAGE_GBM_BUFFER;
+
    /* This is true so far, but maybe gbm_bo_get_caching_type is needed in the future. */
    if (!strcmp(gbm_device_get_backend_name(gbm->device), "i915"))
       gr->map_info = VIRGL_RENDERER_MAP_CACHE_CACHED;
@@ -8272,7 +8274,6 @@ static void vrend_resource_gbm_init(struct vrend_resource *gr, uint32_t format)
       return;
    }
 
-   gr->storage_bits |= VREND_STORAGE_GBM_BUFFER;
    gr->storage_bits |= VREND_STORAGE_EGL_IMAGE;
 
 #else