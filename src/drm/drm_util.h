Vera-Firefly/Virglrenderer-test

commit 060f448180308c93ded9595c7e4a04004f2a1ae1

drm: Silence GCC warnings
Silence noisy GCC warnings when compiling "linux/overflow.h". Diagnostic
pragmas used in a case of clang are not enough for GCC. There is no need
to suppress compiler warnings globally, hence limit the scope to overflow.h
only.

Signed-off-by: Dmitry Osipenko <dmitry.osipenko@collabora.com>
Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/1127>

src/drm/drm_util.h
@@ -6,12 +6,22 @@
 #ifndef DRM_UTIL_H_
 #define DRM_UTIL_H_
 
+#pragma GCC diagnostic push
+
+#if defined(__clang__)
 #pragma GCC diagnostic ignored "-Wgnu-zero-variadic-macro-arguments"
 #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
 #pragma GCC diagnostic ignored "-Wlanguage-extension-token"
 #pragma GCC diagnostic ignored "-Wgnu-statement-expression"
+#endif
+
+#if defined(__GNUC__)
+#pragma GCC diagnostic ignored "-Wpedantic"
+#endif
 
 #include "linux/overflow.h"
+
+#pragma GCC diagnostic pop
 
 void _drm_log(const char *fmt, ...);
 #define drm_log(fmt, ...) _drm_log("%s:%d: " fmt, __func__, __LINE__, ##__VA_ARGS__)

commit e51d1ddea0a60969154d1d2f7996c92d3e605006

drm: fix build failure and misc .clang-format issue
drm_log relies on -Wgnu-zero-variadic-macro-arguments ignored.

Fixes: 060f4481 ("drm: Silence GCC warnings")
Signed-off-by: Yiwei Zhang <zzyiwei@chromium.org>
Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/1165>

src/drm/.clang-format
@@ -19,7 +19,6 @@ BreakBeforeBraces: Custom
 ColumnLimit: 90
 ContinuationIndentWidth: 3
 Cpp11BracedListStyle: false
-Cpp11BracedListStyle: true
 BreakStringLiterals: false
 ForEachMacros:
   - BITSET_FOREACH_SET

src/drm/drm_util.h
@@ -21,10 +21,10 @@
 
 #include "linux/overflow.h"
 
-#pragma GCC diagnostic pop
-
 void _drm_log(const char *fmt, ...);
 #define drm_log(fmt, ...) _drm_log("%s:%d: " fmt, __func__, __LINE__, ##__VA_ARGS__)
+
+#pragma GCC diagnostic pop
 
 #if 0
 #define drm_dbg drm_log