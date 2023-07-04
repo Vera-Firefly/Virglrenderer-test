c965f45cc290eb6d1009723db6c04fe6c042f658
Julia Zhang <julia.zhang@amd.com>
2023-07-04 16:06:31 +0800

    vrend: Increate host_feature_check_version
    Increate caps->v2.host_feature_check_version to support newer Mesa work
    with older viglrenderer on importing blob resource.
    
    Signed-off-by: Julia Zhang <julia.zhang@amd.com>
    Reviewed-by: Corentin Noël <corentin.noel@collabora.com>
    Reviewed-by: Gert Wollny <gert.wollny@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/1068>

--- src/vrend_renderer.c
+++ src/vrend_renderer.c
@@ -11839,7 +11839,7 @@ static void vrend_renderer_fill_caps_v2(int gl_ver, int gles_ver,  union virgl_c
     * this value to avoid regressions when a guest with a new mesa version is
     * run on an old virgl host. Use it also to indicate non-cap fixes on the
     * host that help enable features in the guest. */
-   caps->v2.host_feature_check_version = 17;
+   caps->v2.host_feature_check_version = 18;
 
    /* Forward host GL_RENDERER to the guest. */
    strncpy(caps->v2.renderer, renderer, sizeof(caps->v2.renderer) - 1);
