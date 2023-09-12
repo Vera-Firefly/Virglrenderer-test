bbb844c0a6df799850cea696bba7f50d028b8732
Gert Wollny <gert.wollny@collabora.com>
2023-09-12 13:47:47 +0200

    tests: initialise all context parameters
    Fixes: a68a262 (Add support for requesting a compat profile)
    
    Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/1234>

--- tests/test_fuzzer_formats.c
+++ tests/test_fuzzer_formats.c
@@ -57,7 +57,7 @@ static virgl_renderer_gl_context
 fuzzer_create_gl_context(UNUSED void *cookie, UNUSED int scanout_idx,
                          struct virgl_renderer_gl_ctx_param *param)
 {
-   struct virgl_gl_ctx_param vparams;
+   struct virgl_gl_ctx_param vparams = {0};
    vparams.shared = false;
    vparams.major_ver = param->major_ver;
    vparams.minor_ver = param->minor_ver;
