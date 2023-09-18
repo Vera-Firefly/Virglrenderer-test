5ec8b8e4cddd6d62b93c8145a777d6eb3f3e9c66
Corentin Noël <corentin.noel@collabora.com>
2023-09-18 16:27:57 +0200

    resource: virgl_resource_create_from_fd always take the ownership of the fd
    Make the failing case also close the file descriptor.
    This allows consumers to know that the fd is actually given away.

--- src/virgl_resource.c
+++ src/virgl_resource.c
@@ -143,8 +143,10 @@ virgl_resource_create_from_fd(uint32_t res_id,
    assert(fd_type != VIRGL_RESOURCE_FD_INVALID  && fd >= 0);
 
    res = virgl_resource_create(res_id);
-   if (!res)
+   if (!res) {
+      close(fd);
       return NULL;
+   }
 
    res->fd_type = fd_type;
    /* take ownership */
