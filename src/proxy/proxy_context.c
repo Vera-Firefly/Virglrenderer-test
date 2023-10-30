f5fe0309c07c871020407ecbf7ef7b867712fc36
Yiwei Zhang <zzyiwei@chromium.org>
2023-10-30 09:56:51 -0700

    proxy: turn resource type asserts into fail paths
    ..otherwise importing the unsupported shm type resource would end up
    with a seg fault.
    
    Reported-by: Andrew D. Gazizov <andrew.gazizov@opensynergy.com>
    Signed-off-by: Yiwei Zhang <zzyiwei@chromium.org>
    Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/1288>

--- src/proxy/proxy_context.c
+++ src/proxy/proxy_context.c
@@ -447,22 +447,26 @@ proxy_context_attach_resource(struct virgl_context *base, struct virgl_resource 
     * for importing external Vulkan opaque resource. For shm, we only create with blob_id
     * 0 via CREATE_RESOURCE above.
     */
-   assert(res->fd_type == VIRGL_RESOURCE_FD_INVALID ||
-          res->fd_type == VIRGL_RESOURCE_FD_DMABUF);
+   if (res->fd_type != VIRGL_RESOURCE_FD_INVALID &&
+       res->fd_type != VIRGL_RESOURCE_FD_DMABUF) {
+      proxy_log("failed to attach res %d with fd_type %d", res_id, res->fd_type);
+      return;
+   }
 
    enum virgl_resource_fd_type res_fd_type = res->fd_type;
    int res_fd = res->fd;
    uint64_t res_size = res->map_size;
    bool close_res_fd = false;
    if (res_fd_type == VIRGL_RESOURCE_FD_INVALID) {
+      /* importable pipe resouce can only export as dma-buf */
       res_fd_type = virgl_resource_export_fd(res, &res_fd);
-      if (res_fd_type == VIRGL_RESOURCE_FD_INVALID) {
-         proxy_log("failed to export res %d", res_id);
+      if (res_fd_type != VIRGL_RESOURCE_FD_DMABUF) {
+         /* close fd for unexpected fd type from succeeded export */
+         if (res_fd_type != VIRGL_RESOURCE_FD_INVALID)
+            close(res_fd);
+         proxy_log("exported res %d to unexpected fd_type %d", res_id, res_fd_type);
          return;
       }
-
-      /* importable pipe resouce can only export as dma-buf */
-      assert(res_fd_type == VIRGL_RESOURCE_FD_DMABUF);
 
       /* get the actual dma-buf size here because:
        * - pipe resource created by vrend has a zero map_size
